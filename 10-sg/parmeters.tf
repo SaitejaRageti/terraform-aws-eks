cafcb38ed8843d5f8e9b8416a7f621c5e631248b
saitejarageti <saitejarageti129@gmail.com>
2025-09-08 16:06:32 +0530

    jenkins-terraform

--- 10-sg/parmeters.tf
+++ 10-sg/parmeters.tf
@@ -12,12 +12,12 @@ resource "aws_ssm_parameter" "bastion_sg_id" {
 
 resource "aws_ssm_parameter" "eks_control_plane_sg_id" {
   name = "/${var.project}-${var.environment}/eks_control_plane_sg_id"
-  type = "string"
+  type = "String"
   value = module.eks_control_plane.sg_id
 }
 
 resource "aws_ssm_parameter" "eks_node_sg_id" {
   name = "/${var.project}-${var.environment}/eks_node_sg_id"
-  type = "string"
+  type = "String"
   value = module.eks_node.sg_id
 }
